d7a70d99be26152f227c2e074b45c9b05b629537
Nahiyan Khan Nirob <nahiyannirob565@gmail.com>
2026-04-06 11:29:14 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# e_mart
+This is  a ecormarce platform 
---
+++ lib/consts/consts.dart
@@ -0,0 +1,5 @@
+export 'colors.dart';
+export '../consts/consts.dart';
+export '../consts/images.dart';
+export '../consts/styles.dart';
+export 'package:velocity_x/velocity_x.dart';
